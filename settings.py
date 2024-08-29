3cfa9e4f8a814c5d165a333b8d06918eacef72ff
Brady <bradygovender14@gmail.com>
2024-08-29 23:32:49 +0200

    Commit #5 - updating the settings.py file to download the file to a different directory and rebuilding the project.

--- settings.py
+++ settings.py
@@ -1,8 +1,17 @@
+from pathlib import Path
+
+# Function to get the Downloads directory path
+def get_download_dir():
+    # Get the home directory of the current user
+    home_dir = Path.home()
+    # Construct the path to the Downloads folder
+    download_dir = home_dir / "Downloads" / "YouTubeDownloader"
+    return download_dir
+
 # Directory for downloads
-DOWNLOAD_DIR = 'D:\\python mp3'  # Set the directory where the videos will be saved
+DOWNLOAD_DIR = str(get_download_dir())  # Set the directory where the videos will be saved
 
 # System configuration settings
 def configure_system():
     from pydub import AudioSegment  # Import AudioSegment for audio conversion
-    # Set the path to ffmpeg if it's not in the system PATH
-    AudioSegment.ffmpeg = "path_to_your_ffmpeg_executable"  # Update this to the correct path on your system
+
